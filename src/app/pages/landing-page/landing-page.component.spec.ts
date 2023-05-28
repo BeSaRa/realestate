c21bae0037e0f721b4dba85a9b98ab05bfbe398a
BeSaRa <ahmedbesara@gmailcom>
2023-05-28 14:15:48 +0300

    add jest to the project

--- src/app/pages/landing-page/landing-page.component.spec.ts
+++ src/app/pages/landing-page/landing-page.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LandingPageComponent } from './landing-page.component';
+import LandingPageComponent from './landing-page.component';
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
@@ -8,7 +8,7 @@ describe('LandingPageComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [LandingPageComponent]
+      imports: [LandingPageComponent],
     });
     fixture = TestBed.createComponent(LandingPageComponent);
     component = fixture.componentInstance;
