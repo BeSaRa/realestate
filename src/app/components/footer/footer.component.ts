43773b61e863ba93d4bf0e65f637327ca0ee2e5b
BeSaRa <ahmedbesara@gmailcom>
2023-05-30 05:19:21 +0300

    finish full footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, NgOptimizedImage],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
