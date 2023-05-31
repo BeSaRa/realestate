fd5053da6ce915d2ec0106ccb60df26970e3b913
AmeerShahoud <amir.shahood.1997@gmail.com>
2023-05-31 03:54:15 -0700

    creating news page

--- src/app/components/news-list/news-list.component.ts
+++ src/app/components/news-list/news-list.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { NewsItemComponent } from '../news-item/news-item.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-news-list',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, NewsItemComponent],
+  imports: [CommonModule, RouterModule, MatButtonModule, NewsItemComponent],
   templateUrl: './news-list.component.html',
   styleUrls: ['./news-list.component.scss'],
 })
