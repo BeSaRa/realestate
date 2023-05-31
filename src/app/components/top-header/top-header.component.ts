4655720d404f2ed324afba74213d4ff5737963bf
AmeerShahoud <amir.shahood.1997@gmail.com>
2023-05-31 02:16:10 -0700

    making some tiny edits

--- src/app/components/top-header/top-header.component.ts
+++ src/app/components/top-header/top-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -10,4 +10,9 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './top-header.component.html',
   styleUrls: ['./top-header.component.scss'],
 })
-export class TopHeaderComponent {}
+export class TopHeaderComponent {
+
+  toggleFilter(){
+    document.documentElement.classList.toggle('root-filter')
+  }
+}
