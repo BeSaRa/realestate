9d0a622ac5c0d8ae65618e3d0f47feb3479c45d7
AmeerShahoud <amir.shahood.1997@gmail.com>
2023-06-02 07:54:53 -0700

    finish: inquiry functionality

---
+++ src/app/components/inwani/inwani.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InwaniComponent } from './inwani.component';
+
+describe('InwaniComponent', () => {
+  let component: InwaniComponent;
+  let fixture: ComponentFixture<InwaniComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [InwaniComponent]
+    });
+    fixture = TestBed.createComponent(InwaniComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/directives/element-reference.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ElementReferenceDirective } from './element-reference.directive';
+
+describe('ElementReferenceDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ElementReferenceDirective();
+    expect(directive).toBeTruthy();
+  });
+});
