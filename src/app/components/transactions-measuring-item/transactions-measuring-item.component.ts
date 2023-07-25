56c143f4480dd36baabdc1c606175750d3071445
AmeerShahoud <amir.shahood.1997@gmail.com>
2023-07-25 04:44:05 -0700

    feat: add transactions-list carousel

--- src/app/components/transactions-measuring-item/transactions-measuring-item.component.ts
+++ src/app/components/transactions-measuring-item/transactions-measuring-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, HostBinding, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TransactionsMeasuringContract } from '@contracts/transactions-measuring-contract';
 import { TranslationService } from '@services/translation.service';
