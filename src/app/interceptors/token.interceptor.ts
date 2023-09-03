ff4b278f0912b736bd1c9dcdb9f194f523d6db7c
BeSaRa <ahmedbesara@gmailcom>
2023-09-03 15:12:06 +0300

    update translation and token header

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const token = 'IdBqvNPFwKogCLFZh_NGbJy3T91O_kjO';
+    const token = 'lyHWSTHj1SBm9IRECnLAHviNHnXGaS27';
 
     request = request.clone({
       setHeaders: {
