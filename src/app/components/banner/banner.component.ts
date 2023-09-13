1d69a35b600bb5bd7c6406310445c573ce838e73
Modar Eskandar <modar.a.eskandar@gmail.com>
2023-09-13 09:21:21 +0300

    landing page responsive

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,8 +1,10 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonComponent } from '@components/button/button.component';
 import { TranslationService } from '@services/translation.service';
+import { takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-banner',
@@ -13,4 +15,24 @@ import { TranslationService } from '@services/translation.service';
 })
 export class BannerComponent {
   lang = inject(TranslationService);
+  currentScreenSize: 'sm' | 'md' | 'lg' | 'xl' = 'md';
+  buttonSizeMap = new Map<string, 'sm' | 'md' | 'lg' | 'xl'>([
+    [Breakpoints.Small, 'sm'],
+    [Breakpoints.Medium, 'md'],
+    [Breakpoints.Large, 'lg'],
+    [Breakpoints.XLarge, 'xl'],
+  ]);
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver
+      .observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large, Breakpoints.XLarge])
+      // .pipe(takeUntil(this.destroyed))
+      .subscribe((result) => {
+        for (const query of Object.keys(result.breakpoints)) {
+          if (result.breakpoints[query]) {
+            console.log(this.buttonSizeMap.get(query));
+            this.currentScreenSize = this.buttonSizeMap.get(query) ?? 'lg';
+          }
+        }
+      });
+  }
 }
