d2585f2a13ffbaec5eefd574552963d21fca11ea
AmeerShahoud <amir.shahood.1997@gmail.com>
2023-09-13 13:49:07 -0700

    fix some responsive issues

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,10 +1,8 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonComponent } from '@components/button/button.component';
 import { TranslationService } from '@services/translation.service';
-import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-banner',
