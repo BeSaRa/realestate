cd2eb76d9c2ad11bab13238ac2d7e63aefc1c4ec
AmeerShahoud <amir.shahood.1997@gmail.com>
2023-09-14 06:28:25 -0700

    fixing main header lang change issue

--- src/app/components/main-header/main-header.component.ts
+++ src/app/components/main-header/main-header.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
 import { ButtonComponent } from '@components/button/button.component';
 import { IconButtonComponent } from '@components/icon-button/icon-button.component';
@@ -18,15 +17,15 @@ export class MainHeaderComponent {
   lang = inject(TranslationService);
   sticky = inject(StickyService);
 
-  links: { link: string; label: string }[] = [
-    { link: '/home', label: this.lang.map.home },
-    { link: '/sell-indicators', label: this.lang.map.sell_indicator },
-    { link: '/mortgage-indicators', label: this.lang.map.mortgage_indicator },
-    { link: '/rental-indicators', label: this.lang.map.rental_indicator },
-    { link: '/ownership-indicators', label: this.lang.map.ownership_indicator },
-    { link: '/news', label: this.lang.map.news },
-    { link: '/about', label: this.lang.map.about_us },
-    { link: '/laws', label: this.lang.map.laws_and_decisions },
+  links: { link: string; label: () => string }[] = [
+    { link: '/home', label: () => this.lang.map.home },
+    { link: '/sell-indicators', label: () => this.lang.map.sell_indicator },
+    { link: '/mortgage-indicators', label: () => this.lang.map.mortgage_indicator },
+    { link: '/rental-indicators', label: () => this.lang.map.rental_indicator },
+    { link: '/ownership-indicators', label: () => this.lang.map.ownership_indicator },
+    { link: '/news', label: () => this.lang.map.news },
+    { link: '/about', label: () => this.lang.map.about_us },
+    { link: '/laws', label: () => this.lang.map.laws_and_decisions },
   ];
 
   changeLang(event: Event) {
