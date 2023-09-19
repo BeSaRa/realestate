1d48e29b25a9887fb18d46dd81952da7191a4099
werad.saoud <werad.saoud@gmail.com>
2023-09-19 11:05:27 +0300

    Bug 58939, Bug 58941, Bug 58936

--- src/app/pages/mortgage-indicators/mortgage-indicators.component.ts
+++ src/app/pages/mortgage-indicators/mortgage-indicators.component.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { FormControl, ReactiveFormsModule, UntypedFormBuilder } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatOptionModule } from '@angular/material/core';
 import { MatTableModule } from '@angular/material/table';
+import { DateAdapter, MatOptionModule } from '@angular/material/core';
 import { ChartOptions } from '@app-types/ChartOptions';
 import { ButtonComponent } from '@components/button/button.component';
 import { ExtraHeaderComponent } from '@components/extra-header/extra-header.component';
@@ -20,7 +20,10 @@ import { TableColumnTemplateDirective } from '@directives/table-column-template.
 import { ChartType } from '@enums/chart-type';
 import { CriteriaType } from '@enums/criteria-type';
 import { DurationEndpoints } from '@enums/durations';
+import { MortgageCharts } from '@enums/mortgage-charts';
 import { TransactionType } from '@enums/transaction-type';
+import { KpiBaseModel } from '@models/kpi-base-model';
+import { KpiDurationModel } from '@models/kpi-duration-model';
 import { KpiModel } from '@models/kpi-model';
 import { KpiRoot } from '@models/kpiRoot';
 import { MortgageTransaction } from '@models/mortgage-transaction';
@@ -70,6 +73,7 @@ export default class MortgageIndicatorsComponent implements OnInit {
 
   enableChangeAreaMinMaxValues = true;
   enableChangerealEstateValueMinMaxValues = true;
+  adapter = inject(DateAdapter);
 
   criteria: { criteria: CriteriaContract; type: CriteriaType } = {} as {
     criteria: CriteriaContract;
@@ -80,7 +84,7 @@ export default class MortgageIndicatorsComponent implements OnInit {
   fb = inject(UntypedFormBuilder);
 
   @ViewChild('chart', { static: true }) transactionCountChart!: ChartComponent;
-
+  @ViewChild('chartUnitCount', { static: true }) unitCountChart!: ChartComponent;
   @ViewChild('chartValues', { static: true }) transactionValueChart!: ChartComponent;
 
   municipalities = this.lookupService.mortLookups.municipalityList;
@@ -177,13 +181,18 @@ export default class MortgageIndicatorsComponent implements OnInit {
     }),
   ];
   protected readonly DurationTypes = DurationEndpoints;
+  protected readonly ChartsIndicators = MortgageCharts;
 
-  transactionCount?: Record<number, KpiModel[]>;
-  transactionValues?: Record<number, KpiModel[]>;
+  transactionCount?: Record<string, KpiDurationModel[]>;
+  unitCount?: Record<string, KpiDurationModel[]>;
+  transactionValues?: Record<string, KpiDurationModel[]>;
   countChartType: ChartType = ChartType.LINE;
+  unitCountChartType: ChartType = ChartType.LINE;
   valueChartType: ChartType = ChartType.LINE;
 
   transactionCountDuration = DurationEndpoints.YEARLY;
+  transactionValueDuration = DurationEndpoints.YEARLY;
+  unitsCountDuration = DurationEndpoints.YEARLY;
 
   protected readonly ChartType = ChartType;
 
@@ -217,6 +226,35 @@ export default class MortgageIndicatorsComponent implements OnInit {
     tooltip: {
       theme: 'light',
       shared: true,
+      y: [
+        {
+          title: {
+            formatter: (seriesName) => {
+              console.log('first series name: ', seriesName);
+              return seriesName;
+            },
+          },
+          formatter: (val) => val?.toString(),
+        },
+        {
+          title: {
+            formatter: (seriesName) => {
+              console.log('second series name: ', seriesName);
+              return seriesName;
+            },
+          },
+          formatter: (val) => val?.toString(),
+        },
+        {
+          title: {
+            formatter: (seriesName) => {
+              console.log('third series name: ', seriesName);
+              return seriesName;
+            },
+          },
+          formatter: (val) => val?.toString(),
+        },
+      ],
     },
     plotOptions: {
       bar: {
@@ -229,6 +267,81 @@ export default class MortgageIndicatorsComponent implements OnInit {
     yaxis: {
       title: {
         text: this.lang.map.number_of_transactions,
+      },
+    },
+  };
+
+  // total_mortgage_transactions
+  chartUnitCountOptions: Partial<ChartOptions> = {
+    series: [],
+    chart: {
+      height: 350,
+      type: ChartType.LINE,
+    },
+    colors: ['#A29475', '#8A1538'],
+    dataLabels: {
+      enabled: true,
+      style: { colors: ['#259C80'] },
+    },
+    legend: {
+      fontFamily: 'inherit',
+    },
+    stroke: {
+      show: true,
+    },
+    grid: {
+      row: {
+        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+        opacity: 0.5,
+      },
+    },
+    xaxis: {
+      categories: [],
+    },
+    tooltip: {
+      theme: 'light',
+      shared: true,
+      y: [
+        {
+          title: {
+            formatter: (seriesName) => {
+              console.log('first series name: ', seriesName);
+              return seriesName;
+            },
+          },
+          formatter: (val) => val?.toString(),
+        },
+        {
+          title: {
+            formatter: (seriesName) => {
+              console.log('second series name: ', seriesName);
+              return seriesName;
+            },
+          },
+          formatter: (val) => val?.toString(),
+        },
+        {
+          title: {
+            formatter: (seriesName) => {
+              console.log('third series name: ', seriesName);
+              return seriesName;
+            },
+          },
+          formatter: (val) => val?.toString(),
+        },
+      ],
+    },
+    plotOptions: {
+      bar: {
+        columnWidth: '30%',
+        dataLabels: {
+          position: 'top',
+        },
+      },
+    },
+    yaxis: {
+      title: {
+        text: this.lang.map.number_of_units,
       },
     },
   };
@@ -305,6 +418,7 @@ export default class MortgageIndicatorsComponent implements OnInit {
       this.loadTransactions();
 
       this.loadMortgageTransactionChart();
+      this.loadMortgageUnitCountChart();
       this.loadMortgageTransactionValueChart();
     });
   }
@@ -313,40 +427,53 @@ export default class MortgageIndicatorsComponent implements OnInit {
     this.dashboardService
       .loadMortgageTransactionCountChart(this.criteria.criteria, this.transactionCountDuration)
       .subscribe((value) => {
-        this.transactionCount = value;
+        this.transactionCount = this.arrangeMortgageChartsData(value);
         console.log({ transactionCount: value });
         this.updateTransactionCountChart();
       });
   }
 
   loadMortgageTransactionValueChart(): void {
-    this.dashboardService.loadMortgageTransactionValueChart(this.criteria.criteria).subscribe((value) => {
-      this.transactionValues = value;
+    this.dashboardService.loadMortgageTransactionValueChart(this.criteria.criteria, this.transactionValueDuration).subscribe((value) => {
+      this.transactionValues = this.arrangeMortgageChartsData(value);
       console.log({ transactionValues: value });
       this.updateTransactionValueChart();
     });
   }
 
+  loadMortgageUnitCountChart(): void {
+    this.dashboardService
+      .loadMortgageUnitCountChart(this.criteria.criteria, this.unitsCountDuration)
+      .subscribe((value) => {
+        this.unitCount = this.arrangeMortgageChartsData(value);
+        console.log({ unitCount: value });
+        this.updateUnitCountChart();
+      });
+  }
+
   updateTransactionCountChart(): void {
     if (!this.transactionCount) return;
 
-    const xaxis = Object.keys(this.transactionCount);
-    const mort = xaxis.reduce((acc, year) => {
+    const xaxis =
+      Object.keys(this.transactionCount).length > 8
+        ? Object.keys(this.transactionCount).slice(-8)
+        : Object.keys(this.transactionCount);
+    let mort = xaxis.reduce((acc, year) => {
       // console.log(this.lineChartData[year]);
       return [...acc].concat(
         (this.transactionCount &&
-          this.transactionCount[Number(year)].filter((item) => item.actionType === TransactionType.MORTGAGE)) ||
+          this.transactionCount[year].filter((item) => item.actionType === TransactionType.MORTGAGE)) ||
           []
       );
-    }, [] as KpiModel[]);
-    const sell = xaxis.reduce((acc, year) => {
+    }, [] as KpiDurationModel[]);
+    let sell = xaxis.reduce((acc, year) => {
       // console.log(this.lineChartData[year]);
       return [...acc].concat(
         (this.transactionCount &&
-          this.transactionCount[Number(year)].filter((item) => item.actionType === TransactionType.SELL)) ||
+          this.transactionCount[year].filter((item) => item.actionType === TransactionType.SELL)) ||
           []
       );
-    }, [] as KpiModel[]);
+    }, [] as KpiDurationModel[]);
 
     this.transactionCountChart
       .updateOptions({
@@ -359,43 +486,146 @@ export default class MortgageIndicatorsComponent implements OnInit {
             name: this.lang.map.sell,
             data: sell.map((i) => i.kpiVal),
           },
+          {
+            name: 'YoY of' + this.lang.map.mortgage,
+            data: mort.map((i) => i.kpiP2PYoY),
+          },
         ],
         xaxis: {
           categories: xaxis,
         },
       })
-      .then();
+      .then(() => {
+        this.transactionCountChart.hideSeries('YoY of' + this.lang.map.mortgage);
+        // this.transactionCountChart.series;
+      });
   }
 
-  updateTransactionValueChart(): void {
-    if (!this.transactionValues) return;
-    const _minMaxAvg = minMaxAvg(
-      Object.keys(this.transactionValues).map(
-        (year) => (this.transactionValues && this.transactionValues[year as unknown as number][0].kpiVal) || 0
-      )
-    );
-    this.transactionValueChart
+  updateUnitCountChart(): void {
+    if (!this.unitCount) return;
+
+    const xaxis =
+      Object.keys(this.unitCount).length > 8 ? Object.keys(this.unitCount).slice(-8) : Object.keys(this.unitCount);
+    let mort = xaxis.reduce((acc, year) => {
+      // console.log(this.lineChartData[year]);
+      return [...acc].concat(
+        (this.unitCount &&
+          this.unitCount[year].filter((item) => true /*item.actionType === TransactionType.MORTGAGE*/)) ||
+          []
+      );
+    }, [] as KpiDurationModel[]);
+    let sell = xaxis.reduce((acc, year) => {
+      // console.log(this.lineChartData[year]);
+      return [...acc].concat(
+        (this.unitCount && this.unitCount[year].filter((item) => item.actionType === TransactionType.SELL)) || []
+      );
+    }, [] as KpiDurationModel[]);
+
+    console.log('updateUnitCountChart mort: ', mort);
+    console.log('updateUnitCountChart sell: ', sell);
+
+    this.unitCountChart
       .updateOptions({
-        colors: [this.appChartTypesService.chartColorsFormatter(_minMaxAvg)],
         series: [
           {
             name: this.lang.map.mortgage,
-            data: Object.keys(this.transactionValues).map((year: string) => {
-              return {
-                x: year,
-                y: this.transactionValues && this.transactionValues[year as unknown as number][0].kpiVal,
-              };
-            }),
+            data: mort.map((i) => i.kpiVal),
+          },
+          {
+            name: this.lang.map.sell,
+            data: sell.map((i) => i.kpiVal),
+          },
+          {
+            name: 'YoY of' + this.lang.map.mortgage,
+            data: mort.map((i) => i.kpiP2PYoY),
           },
         ],
+        xaxis: {
+          categories: xaxis,
+        },
       })
-      .then();
+      .then(() => {
+        this.unitCountChart.hideSeries('YoY of' + this.lang.map.mortgage);
+      });
+  }
+
+  updateTransactionValueChart(): void {
+
+    if (!this.transactionValues) return;
+
+    const xaxis =
+      Object.keys(this.transactionValues).length > 8 ? Object.keys(this.transactionValues).slice(-8) : Object.keys(this.transactionValues);
+    let mort = xaxis.reduce((acc, year) => {
+      // console.log(this.lineChartData[year]);
+      return [...acc].concat(
+        (this.transactionValues &&
+          this.transactionValues[year].filter((item) => true /*item.actionType === TransactionType.MORTGAGE*/)) ||
+          []
+      );
+    }, [] as KpiDurationModel[]);
+    let sell = xaxis.reduce((acc, year) => {
+      // console.log(this.lineChartData[year]);
+      return [...acc].concat(
+        (this.transactionValues && this.transactionValues[year].filter((item) => item.actionType === TransactionType.SELL)) || []
+      );
+    }, [] as KpiDurationModel[]);
+
+    console.log('updateTransactionValueChart mort: ', mort);
+    console.log('updateTransactionValueChart sell: ', sell);
+
+    this.transactionValueChart
+      .updateOptions({
+        series: [
+          {
+            name: this.lang.map.mortgage,
+            data: mort.map((i) => i.kpiVal),
+          },
+          {
+            name: this.lang.map.sell,
+            data: sell.map((i) => i.kpiVal),
+          },
+          {
+            name: 'YoY of' + this.lang.map.mortgage,
+            data: mort.map((i) => i.kpiP2PYoY),
+          },
+        ],
+        xaxis: {
+          categories: xaxis,
+        },
+      })
+      .then(() => {
+        this.transactionValueChart.hideSeries('YoY of' + this.lang.map.mortgage);
+      });
+
+
+    // if (!this.transactionValues) return;
+    // const _minMaxAvg = minMaxAvg(
+    //   Object.keys(this.transactionValues).map(
+    //     (year) => (this.transactionValues && this.transactionValues[year as unknown as number][0].kpiVal) || 0
+    //   )
+    // );
+    // this.transactionValueChart
+    //   .updateOptions({
+    //     colors: [this.appChartTypesService.chartColorsFormatter(_minMaxAvg)],
+    //     series: [
+    //       {
+    //         name: this.lang.map.mortgage,
+    //         data: Object.keys(this.transactionValues).map((year: string) => {
+    //           return {
+    //             x: year,
+    //             y: this.transactionValues && this.transactionValues[year as unknown as number][0].kpiVal,
+    //           };
+    //         }),
+    //       },
+    //     ],
+    //   })
+    //   .then();
   }
 
   updateChartType(
     type: ChartType,
-    chart: 'transactionCountChart' | 'transactionValueChart',
-    chartProperty: 'countChartType' | 'valueChartType'
+    chart: 'transactionCountChart' | 'transactionValueChart' | 'unitCountChart',
+    chartProperty: 'countChartType' | 'valueChartType' | 'unitCountChartType'
   ) {
     this[chart]
       .updateOptions({
@@ -406,13 +636,58 @@ export default class MortgageIndicatorsComponent implements OnInit {
     this[chartProperty] = type;
   }
 
-  isSelectedChartType(type: ChartType, chartProperty: 'countChartType' | 'valueChartType') {
+  isSelectedChartType(type: ChartType, chartProperty: 'countChartType' | 'valueChartType' | 'unitCountChartType') {
     return this[chartProperty] === type;
   }
 
-  updateChartDuration(durationType: DurationEndpoints) {
-    this.transactionCountDuration = durationType;
-    this.loadMortgageTransactionChart();
+  updateChartDuration(durationType: DurationEndpoints, chartIndicator: string) {
+    switch (chartIndicator) {
+      case this.ChartsIndicators.TransactionsNumber:
+        this.transactionCountDuration = durationType;
+        this.loadMortgageTransactionChart();
+        break;
+      case this.ChartsIndicators.UnitsNumber:
+        this.unitsCountDuration = durationType;
+        this.loadMortgageUnitCountChart();
+        break;
+      case this.ChartsIndicators.TransactionsValues:
+        this.transactionValueDuration = durationType;
+        this.loadMortgageTransactionValueChart();
+        break;
+      default:
+        break;
+    }
+  }
+
+  arrangeMortgageChartsData(values: KpiDurationModel[]): Record<string, KpiDurationModel[]> {
+    return values.reduce((acc, item) => {
+      if (item.issueYear) {
+        if (
+          !Object.prototype.hasOwnProperty.call(
+            acc,
+            !item.issuePeriod
+              ? item.issueYear.toString()
+              : item.issueYear.toString() + '_' + item.issuePeriod.toString()
+          )
+        ) {
+          !item.issuePeriod
+            ? (acc[item.issueYear] = [])
+            : (acc[item.issueYear.toString() + '_' + item.issuePeriod.toString()] = []);
+        }
+        !item.issuePeriod
+          ? acc[item.issueYear].push(item)
+          : acc[item.issueYear.toString() + '_' + item.issuePeriod.toString()].push(item);
+        return { ...acc };
+      } else {
+        this.adapter.setLocale(this.lang.getCurrent().code === 'ar-SA' ? 'ar-EG' : 'en-US');
+        const months = this.adapter.getMonthNames('long');
+        if (!Object.prototype.hasOwnProperty.call(acc, months[item.issuePeriod - 1])) {
+          acc[months[item.issuePeriod - 1]] = [];
+        }
+        acc[months[item.issuePeriod - 1]].push(item);
+        return { ...acc };
+      }
+    }, {} as Record<string, KpiDurationModel[]>);
   }
 
   private loadTransactions() {
