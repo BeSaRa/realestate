bb013e1eee283eabe0e8e2c37e2ae55c0095540d
werad.saoud <werad.saoud@gmail.com>
2023-09-19 23:11:27 +0300

    logs removed

--- src/app/pages/mortgage-indicators/mortgage-indicators.component.ts
+++ src/app/pages/mortgage-indicators/mortgage-indicators.component.ts
@@ -241,7 +241,6 @@ export default class MortgageIndicatorsComponent implements OnInit {
         {
           title: {
             formatter: (seriesName) => {
-              console.log('first series name: ', seriesName);
               return seriesName;
             },
           },
@@ -250,7 +249,6 @@ export default class MortgageIndicatorsComponent implements OnInit {
         {
           title: {
             formatter: (seriesName) => {
-              console.log('second series name: ', seriesName);
               return seriesName;
             },
           },
@@ -259,7 +257,6 @@ export default class MortgageIndicatorsComponent implements OnInit {
         {
           title: {
             formatter: (seriesName) => {
-              console.log('third series name: ', seriesName);
               return seriesName;
             },
           },
@@ -316,7 +313,6 @@ export default class MortgageIndicatorsComponent implements OnInit {
         {
           title: {
             formatter: (seriesName) => {
-              console.log('first series name: ', seriesName);
               return seriesName;
             },
           },
@@ -325,7 +321,6 @@ export default class MortgageIndicatorsComponent implements OnInit {
         {
           title: {
             formatter: (seriesName) => {
-              console.log('second series name: ', seriesName);
               return seriesName;
             },
           },
@@ -334,7 +329,6 @@ export default class MortgageIndicatorsComponent implements OnInit {
         {
           title: {
             formatter: (seriesName) => {
-              console.log('third series name: ', seriesName);
               return seriesName;
             },
           },
@@ -416,12 +410,8 @@ export default class MortgageIndicatorsComponent implements OnInit {
   ngOnInit() {}
 
   filterChange($event: { criteria: CriteriaContract; type: CriteriaType }): void {
-    console.log('$event: ', $event);
-
     this.criteria = $event;
     this.dashboardService.loadMortgageRoots(this.criteria.criteria).subscribe((values) => {
-      console.log('values: ', values);
-
       this.rootKpis.map((item, index) => {
         item.value = (values[index] && values[index].kpiVal) || 0;
         item.yoy = (values[index] && values[index].kpiYoYVal) || 0;
@@ -439,7 +429,6 @@ export default class MortgageIndicatorsComponent implements OnInit {
       .loadMortgageTransactionCountChart(this.criteria.criteria, this.transactionCountDuration)
       .subscribe((value) => {
         this.transactionCount = this.arrangeMortgageChartsData(value);
-        console.log({ transactionCount: value });
         this.updateTransactionCountChart();
       });
   }
@@ -447,7 +436,6 @@ export default class MortgageIndicatorsComponent implements OnInit {
   loadMortgageTransactionValueChart(): void {
     this.dashboardService.loadMortgageTransactionValueChart(this.criteria.criteria, this.transactionValueDuration).subscribe((value) => {
       this.transactionValues = this.arrangeMortgageChartsData(value);
-      console.log({ transactionValues: value });
       this.updateTransactionValueChart();
     });
   }
@@ -457,7 +445,6 @@ export default class MortgageIndicatorsComponent implements OnInit {
       .loadMortgageUnitCountChart(this.criteria.criteria, this.unitsCountDuration)
       .subscribe((value) => {
         this.unitCount = this.arrangeMortgageChartsData(value);
-        console.log({ unitCount: value });
         this.updateUnitCountChart();
       });
   }
@@ -470,7 +457,6 @@ export default class MortgageIndicatorsComponent implements OnInit {
         ? Object.keys(this.transactionCount).slice(-8)
         : Object.keys(this.transactionCount);
     let mort = xaxis.reduce((acc, year) => {
-      // console.log(this.lineChartData[year]);
       return [...acc].concat(
         (this.transactionCount &&
           this.transactionCount[year].filter((item) => item.actionType === TransactionType.MORTGAGE)) ||
@@ -478,7 +464,6 @@ export default class MortgageIndicatorsComponent implements OnInit {
       );
     }, [] as KpiDurationModel[]);
     let sell = xaxis.reduce((acc, year) => {
-      // console.log(this.lineChartData[year]);
       return [...acc].concat(
         (this.transactionCount &&
           this.transactionCount[year].filter((item) => item.actionType === TransactionType.SELL)) ||
@@ -518,7 +503,6 @@ export default class MortgageIndicatorsComponent implements OnInit {
     const xaxis =
       Object.keys(this.unitCount).length > 8 ? Object.keys(this.unitCount).slice(-8) : Object.keys(this.unitCount);
     let mort = xaxis.reduce((acc, year) => {
-      // console.log(this.lineChartData[year]);
       return [...acc].concat(
         (this.unitCount &&
           this.unitCount[year].filter((item) => true /*item.actionType === TransactionType.MORTGAGE*/)) ||
@@ -526,14 +510,10 @@ export default class MortgageIndicatorsComponent implements OnInit {
       );
     }, [] as KpiDurationModel[]);
     let sell = xaxis.reduce((acc, year) => {
-      // console.log(this.lineChartData[year]);
       return [...acc].concat(
         (this.unitCount && this.unitCount[year].filter((item) => item.actionType === TransactionType.SELL)) || []
       );
     }, [] as KpiDurationModel[]);
-
-    console.log('updateUnitCountChart mort: ', mort);
-    console.log('updateUnitCountChart sell: ', sell);
 
     this.unitCountChart
       .updateOptions({
@@ -567,7 +547,6 @@ export default class MortgageIndicatorsComponent implements OnInit {
     const xaxis =
       Object.keys(this.transactionValues).length > 8 ? Object.keys(this.transactionValues).slice(-8) : Object.keys(this.transactionValues);
     let mort = xaxis.reduce((acc, year) => {
-      // console.log(this.lineChartData[year]);
       return [...acc].concat(
         (this.transactionValues &&
           this.transactionValues[year].filter((item) => true /*item.actionType === TransactionType.MORTGAGE*/)) ||
@@ -575,14 +554,10 @@ export default class MortgageIndicatorsComponent implements OnInit {
       );
     }, [] as KpiDurationModel[]);
     let sell = xaxis.reduce((acc, year) => {
-      // console.log(this.lineChartData[year]);
       return [...acc].concat(
         (this.transactionValues && this.transactionValues[year].filter((item) => item.actionType === TransactionType.SELL)) || []
       );
     }, [] as KpiDurationModel[]);
-
-    console.log('updateTransactionValueChart mort: ', mort);
-    console.log('updateTransactionValueChart sell: ', sell);
 
     this.transactionValueChart
       .updateOptions({
