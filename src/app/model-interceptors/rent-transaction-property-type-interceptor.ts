a5286c61b46499cfde5a1344dae1762cf38d8428
MHM-86 <mohammad.mousa.it@gmail.com>
2023-09-24 15:48:58 +0300

    - Add missing filters to the filters list. (Bug 58813). - Add "Rent indicator Depending on property type" table to Rent Page. - Implement reloading "transaction list" table when filter changes (in All Pages). - Add Sell contracts count according to price range chart.

---
+++ src/app/model-interceptors/rent-transaction-property-type-interceptor.ts
@@ -0,0 +1,30 @@
+import { ModelInterceptorContract } from 'cast-response';
+import { RentTransactionPropertyType } from '@models/rent-transaction-property-type';
+import { ServiceRegistry } from '@services/service-registry';
+import { LookupService } from '@services/lookup.service';
+import { Lookup } from '@models/lookup';
+
+export class RentTransactionPropertyTypeInterceptor implements ModelInterceptorContract<RentTransactionPropertyType> {
+  send(model: Partial<RentTransactionPropertyType>): Partial<RentTransactionPropertyType> {
+    return model;
+  }
+
+  receive(model: RentTransactionPropertyType): RentTransactionPropertyType {
+    const lookupService = ServiceRegistry.get<LookupService>('LookupService');
+
+    // we register an issue that these should not be null. This assign will be removed 
+    // after fixing the issue form be team
+    !model.sumArea && (model.sumArea = 0);
+    !model.rentPaymentMeter && (model.rentPaymentMeter = 0);
+    !model.rentPaymentSqFeet && (model.rentPaymentSqFeet = 0);
+
+    
+    model.propertyTypeInfo = lookupService.rentPropertyTypeMap[model.propertyTypeId] ||
+    new Lookup().clone<Lookup>({
+      arName: `غير معروف ${model.propertyTypeId} `,
+      enName: `Unknown ${model.propertyTypeId}`,
+      lookupKey: model.propertyTypeId,
+    }); // not all lookups provided by be;
+    return model;
+  }
+}
