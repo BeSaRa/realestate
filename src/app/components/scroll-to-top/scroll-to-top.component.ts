c5eec6e3cd03527fa0f14917e0147246af3436d1
MHM-86 <mohammad.mousa.it@gmail.com>
2023-10-04 01:59:05 +0300

    - Fixing Bug 59526: MM QREP - All pages - "back-to-top" button to be added to all pages.

---
+++ src/app/components/scroll-to-top/scroll-to-top.component.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Output, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { SideBarDirection } from '@enums/side-bar-direction';
+
+const MATERIAL_MODULES = [MatButtonModule, MatIconModule];
+
+@Component({
+  standalone: true,
+  imports: [...MATERIAL_MODULES, CommonModule],
+  selector: 'app-scroll-to-top',
+  templateUrl: './scroll-to-top.component.html',
+  styleUrls: ['./scroll-to-top.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ScrollToTopComponent {
+
+  @Input() floating = SideBarDirection.RIGHT;
+  @Output() scrollToTop = new EventEmitter<void>();
+
+  onScrollToTop(): void {
+    this.scrollToTop.emit();
+  }
+  getBackToTopStyles() {
+    return {
+      float: [this.floating]
+    };
+  }
+}
