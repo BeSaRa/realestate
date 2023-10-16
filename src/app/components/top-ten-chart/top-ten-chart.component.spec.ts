7cbdc05fcac480118867c9dcca0b871a10b79498
AmeerShahoud <amir.shahood.1997@gmail.com>
2023-10-16 03:33:38 -0700

    initializing top-10 component

---
+++ src/app/components/top-ten-chart/top-ten-chart.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TopTenChartComponent } from './top-ten-chart.component';
+
+describe('TopTenChartComponent', () => {
+  let component: TopTenChartComponent;
+  let fixture: ComponentFixture<TopTenChartComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [TopTenChartComponent]
+    });
+    fixture = TestBed.createComponent(TopTenChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
