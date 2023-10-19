eb71f276f12dab65b3d98acc6efb6d1e53098c04
AmeerShahoud <amir.shahood.1997@gmail.com>
2023-10-19 03:01:13 -0700

    remove uneeded code

--- src/app/models/sell-top-10-model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SellTop10Model } from './sell-top-10-model';
-
-describe('SellTop10Model', () => {
-  it('should create an instance', () => {
-    expect(new SellTop10Model()).toBeTruthy();
-  });
-});
