d7792fb92d1f7f4183b9969e84fea672e53d8f92
AmeerShahoud <amir.shahood.1997@gmail.com>
2023-10-21 06:40:15 -0700

    creating property-carousel component

---
+++ src/app/components/property-carousel/property-carousel.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PropertyCarouselComponent } from './property-carousel.component';
+
+describe('PropertyCarouselComponent', () => {
+  let component: PropertyCarouselComponent;
+  let fixture: ComponentFixture<PropertyCarouselComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [PropertyCarouselComponent]
+    });
+    fixture = TestBed.createComponent(PropertyCarouselComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
