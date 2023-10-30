c0505f80616932ee5a623729b0c0d56c1bdef3f9
AmeerShahoud <amir.shahood.1997@gmail.com>
2023-10-30 01:27:29 -0700

    creating sell forecasting component initial shape

---
+++ src/app/components/sell-forecasting-chart/sell-forecasting-chart.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SellForecastingChartComponent } from './sell-forecasting-chart.component';
+
+describe('SellForecastingChartComponent', () => {
+  let component: SellForecastingChartComponent;
+  let fixture: ComponentFixture<SellForecastingChartComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [SellForecastingChartComponent]
+    });
+    fixture = TestBed.createComponent(SellForecastingChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
