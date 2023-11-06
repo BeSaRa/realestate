ea71c57200181673a871d260312eee81d2bc2fda
AmeerShahoud <amir.shahood.1997@gmail.com>
2023-11-06 10:11:32 -0800

    creating municipality chart component

---
+++ src/app/components/municipalities-chart/municipalities-chart.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MunicipalitiesChartComponent } from './municipalities-chart.component';
+
+describe('MunicipalitiesChartComponent', () => {
+  let component: MunicipalitiesChartComponent;
+  let fixture: ComponentFixture<MunicipalitiesChartComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [MunicipalitiesChartComponent]
+    });
+    fixture = TestBed.createComponent(MunicipalitiesChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
