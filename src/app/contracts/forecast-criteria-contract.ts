95ab2c5f20594b96dc85cf5617d4880027c7cac7
AmeerShahoud <amir.shahood.1997@gmail.com>
2023-11-12 22:48:38 -0800

    making forecast chart component reusable

--- src/app/contracts/forecast-criteria-contract.ts
+++ src/app/contracts/forecast-criteria-contract.ts
@@ -1,6 +1,19 @@
+import { CriteriaContract } from './criteria-contract';
+import { LangKeysContract } from './lang-keys-contract';
+
 export interface ForecastCriteriaContract {
   municipalityId: number;
   areaCode: number;
   propertyTypeId: number;
   purposeId: number;
+  zoneId: number;
+  streetNo: number;
+  bedRoomsCount: number;
+}
+
+export interface ForecastCriteriaItemContract {
+  key: keyof CriteriaContract;
+  forecastKey: keyof ForecastCriteriaContract;
+  langKey: keyof LangKeysContract;
+  isArray: boolean;
 }
