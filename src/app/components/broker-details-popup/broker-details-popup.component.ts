9c73fab1fc4678459071f126bb354d7ae4028240
AmeerShahoud <amir.shahood.1997@gmail.com>
2023-11-13 11:52:38 -0800

    creating broker and broker details popup components

---
+++ src/app/components/broker-details-popup/broker-details-popup.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Broker } from '@models/broker';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { IconButtonComponent } from '@components/icon-button/icon-button.component';
+import { ButtonComponent } from '@components/button/button.component';
+import { TranslationService } from '@services/translation.service';
+
+@Component({
+  selector: 'app-broker-details-popup',
+  standalone: true,
+  imports: [CommonModule, IconButtonComponent, ButtonComponent],
+  templateUrl: './broker-details-popup.component.html',
+  styleUrls: ['./broker-details-popup.component.scss'],
+})
+export class BrokerDetailsPopupComponent {
+  lang = inject(TranslationService);
+  brokerData: Broker = inject(MAT_DIALOG_DATA);
+  ref = inject(MatDialogRef);
+}
