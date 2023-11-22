b0552425b7ce3f28d761bc1d8c8e002d1a10ebad
AmeerShahoud <amir.shahood.1997@gmail.com>
2023-11-22 14:36:48 -0800

    using click outside directive in sidebar

--- src/app/services/side-bar.service.ts
+++ src/app/services/side-bar.service.ts
@@ -1,18 +1,43 @@
-import { Injectable, computed, signal } from '@angular/core';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { DialogService } from './dialog.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SideBarService {
+  router = inject(Router);
+  dialog = inject(DialogService);
+
   private _isOpened = signal(false);
 
   isOpened = computed(() => this._isOpened());
+  isOpenTriggered = false;
+
+  constructor() {
+    this._listenToRouteChange();
+    this._listenToDialogOpened();
+  }
 
   open() {
+    this.isOpenTriggered = true;
     this._isOpened.set(true);
   }
 
   close() {
+    this.isOpenTriggered = false;
     this._isOpened.set(false);
+  }
+
+  private _listenToRouteChange() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.close();
+      }
+    });
+  }
+
+  private _listenToDialogOpened() {
+    this.dialog.afterDialogOpened$.subscribe(() => this.close());
   }
 }
