15c681de34a76d42d016e7f08940ae576fe495c6
BeSaRa <ahmedbesara@gmailcom>
2023-12-09 17:43:19 +0300

    make interval to animate the slider automatically

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,8 +1,9 @@
-import { Component, ContentChild, Directive, Input, TemplateRef, inject } from '@angular/core';
+import { Component, ContentChild, Directive, Input, TemplateRef, inject, OnInit, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { IconButtonComponent } from '@components/icon-button/icon-button.component';
 import { TranslationService } from '@services/translation.service';
+import { filter, interval, tap } from 'rxjs';
 
 @Directive({
   selector: '[appSliderTemplate]',
@@ -22,7 +23,11 @@ export class SliderTemplateDirective {
     trigger('fade', [state('void', style({ opacity: 0 })), transition('void => *', [animate('0.5s ease-in-out')])]),
   ],
 })
-export class SliderComponent<T> {
+export class SliderComponent<T> implements OnInit {
+  ngOnInit(): void {
+    this.interval$.subscribe();
+  }
+
   @Input({ required: true }) list: T[] = [];
 
   @ContentChild(SliderTemplateDirective) slideTemplateRef!: SliderTemplateDirective;
@@ -31,11 +36,34 @@ export class SliderComponent<T> {
 
   counter = 0;
 
+  isHovered = false;
+
+  interval$ = interval(10000).pipe(
+    filter(() => !this.isHovered),
+    tap(() => {
+      this.hasNext() ? this.showNextSlide() : this.showPreviousSlide();
+    })
+  );
+
   showNextSlide() {
     if (this.counter < this.list.length - 1) this.counter += 1;
   }
 
   showPreviousSlide() {
     if (this.counter > 0) this.counter -= 1;
+  }
+
+  hasNext(): boolean {
+    return this.counter < this.list.length - 1;
+  }
+
+  hasPrev(): boolean {
+    return this.counter > 0;
+  }
+
+  @HostListener('mouseleave')
+  @HostListener('mouseover')
+  toggleHover(): void {
+    this.isHovered = !this.isHovered;
   }
 }
