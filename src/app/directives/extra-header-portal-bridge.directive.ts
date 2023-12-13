c8e44ae10cb3b2e441c18c75e538c9357d3c55a8
AmeerShahoud <amir.shahood.1997@gmail.com>
2023-12-13 05:03:06 -0800

    fix extra header subtitle change issue

--- src/app/directives/extra-header-portal-bridge.directive.ts
+++ src/app/directives/extra-header-portal-bridge.directive.ts
@@ -1,11 +1,20 @@
-import { Directive, Input, OnInit, TemplateRef, ViewContainerRef, inject } from '@angular/core';
+import {
+  Directive,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  TemplateRef,
+  ViewContainerRef,
+  inject,
+} from '@angular/core';
 import { ExtraHeaderPortalBridgeService } from '@services/extra-header-portal-bridge.service';
 
 @Directive({
   selector: '[appExtraHeaderPortal]',
   standalone: true,
 })
-export class ExtraHeaderPortalBridgeDirective implements OnInit {
+export class ExtraHeaderPortalBridgeDirective implements OnChanges, OnInit {
   @Input() extraHeaderSubTitle?: string;
 
   templateRef = inject(TemplateRef, { optional: true });
@@ -13,8 +22,11 @@ export class ExtraHeaderPortalBridgeDirective implements OnInit {
 
   private _portalService = inject(ExtraHeaderPortalBridgeService);
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
     this._portalService.setSubTitle(this.extraHeaderSubTitle);
+  }
+
+  ngOnInit(): void {
     this._portalService.setPortal(this.templateRef, this.viewRef);
   }
 }
