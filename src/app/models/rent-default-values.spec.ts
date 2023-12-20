284adff89ad629d447f2f1b77ba2d78c1b49eb4b
AmeerShahoud <amir.shahood.1997@gmail.com>
2023-12-20 09:33:15 -0800

    remove unused code

--- src/app/models/rent-default-values.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RentDefaultValues } from './rent-default-values';
-
-describe('RentDefaultValues', () => {
-  it('should create an instance', () => {
-    expect(new RentDefaultValues()).toBeTruthy();
-  });
-});
