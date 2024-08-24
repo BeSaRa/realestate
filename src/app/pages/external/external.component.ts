a3c703d0266e727c356a960dc1d17b0519030cb5
Ameer-Shahoud <ameer.shahoud.1997@gmail.com>
2024-08-25 01:40:26 +0300

    adding developer registration success page

--- src/app/pages/external/external.component.ts
+++ src/app/pages/external/external.component.ts
@@ -1,15 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { ButtonComponent } from '@components/button/button.component';
 import { TranslationService } from '@services/translation.service';
 
 @Component({
   selector: 'app-external',
   standalone: true,
-  imports: [CommonModule, RouterModule, RouterOutlet],
+  imports: [CommonModule, RouterModule, RouterOutlet, ButtonComponent],
   templateUrl: './external.component.html',
   styleUrl: './external.component.scss',
 })
 export class ExternalComponent {
   lang = inject(TranslationService);
+
+  changeLang(event: Event) {
+    event.preventDefault();
+    this.lang.toggleLang();
+  }
 }
