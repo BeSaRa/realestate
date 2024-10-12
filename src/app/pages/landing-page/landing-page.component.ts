ad4368ec7f74b4afa161510a2c12979a0978fa40
Ameer-Shahoud <ameer.shahoud.1997@gmail.com>
2024-10-12 12:06:56 +0300

    show only 3 news on landing page

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -48,7 +48,7 @@ export default class LandingPageComponent extends OnDestroyMixin(class {}) {
 
   sliderContent$ = this.dashboardService.loadHomeSliderData(this.urlService.URLS.HOME_SLIDER);
 
-  newsData = this.newsService.load({ limit: 4 });
+  newsData = this.newsService.load({ limit: 3 });
 
   getSliderValue(value: number) {
     return formatNumber(value);
